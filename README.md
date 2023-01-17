ddad604b75d0754114b9cae8b507836172cc0d7c
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-17 16:33:24 +0300

    Got glyph letter to appear on canvas

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # text-art-with-js
+Learn this framework with Bruno Imbrizi on Domestika:
+https://www.domestika.org/en/bruno_imbrizi
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qoec6s)
--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = '1200px sans-serif';
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+
+    context.fillText('E', 0, 0);
+    context.restore();
+
   };
 };
 
