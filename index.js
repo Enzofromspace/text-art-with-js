84629100d0a50fe4c7a1d0ed82944919ba24199f
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-18 15:29:34 +0300

    Created bit map from font letter

--- index.js
+++ index.js
@@ -4,39 +4,97 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+let manager;
+
 let text = 'E';
 let fontSize = 1200;
 let fontFamily = 'trebuchet MS';
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({context, width, height}) => {
+ //setting dimensions for bitmap
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
 
-    context.fillStyle = 'black';
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = 'top';
-   // context.textAlign = 'center'; better method to center below
+    fontSize = cols;
 
-    const metrics = context.measureText(text);
+    typeContext.fillStyle = 'white';
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top';
+   // typecontext.textAlign = 'center'; better method to center below
+
+    const metrics = typeContext.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent; 
 
-    const x = (width - mw) * 0.5 - mx;
-    const y = (height - mh) * 0.5 - my;
+    const tx = (cols - mw) * 0.5 - mx;
+    const ty = (rows - mh) * 0.5 - my;
 
-    context.save();
-    context.translate(x, y); 
+    typeContext.save();
+    typeContext.translate(tx, ty); 
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
   
-    context.fillText(text, 0, 0);
-    context.restore();
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    context.drawImage(typeCanvas, 0, 0);
+    // create loop to display pixel data from array pulled by typeData - log in console to see 
+    for(let i = 0; i < numCells; i++) {
+        const col = i % cols;
+        const row = Math.floor(i / cols);
+
+        const x = col * cell;
+        const y = row * cell;
+
+        const r = typeData[i * 4 + 0];
+        const g = typeData[i * 4 + 1];
+        const b = typeData[i * 4 + 2];
+        const a = typeData[i * 4 + 3];
+
+        context.fillStyle = `rgb(${r},${g},${b})`;
+
+        context.save();
+        context.translate(x,y);
+        context.translate(cell * 0.5, cell * 0.5);
+        //context.fillRect(0, 0, cell, cell);
+        context.beginPath();
+        context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
+        context.fill();
+        
+        context.restore(); 
+    }
   };
 };
+//function to render letter from keyboad click
+const onKeyUp = (e) => { 
+ // console.log(e);
+ text = e.key.toUpperCase();
+ manager.render();
+};
+// listen for keyboard click
+document.addEventListener('keyup', onKeyUp);
 
-canvasSketch(sketch, settings);
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
+
