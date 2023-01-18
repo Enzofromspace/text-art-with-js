ad2d8fd5953d38427209ce6d4c419771afa5646c
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-18 16:01:26 +0300

    letter made from glyph function characters

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [1080, 1080],
@@ -55,7 +56,14 @@ const sketch = ({context, width, height}) => {
 
     const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
     context.drawImage(typeCanvas, 0, 0);
+
     // create loop to display pixel data from array pulled by typeData - log in console to see 
     for(let i = 0; i < numCells; i++) {
         const col = i % cols;
@@ -69,20 +77,34 @@ const sketch = ({context, width, height}) => {
         const b = typeData[i * 4 + 2];
         const a = typeData[i * 4 + 3];
 
-        context.fillStyle = `rgb(${r},${g},${b})`;
+        const glyph = getGlyph(r);
+
+        context.font = `${cell * 2}px ${fontFamily}`;
+        if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+        context.fillStyle = `white`;
 
         context.save();
         context.translate(x,y);
         context.translate(cell * 0.5, cell * 0.5);
         //context.fillRect(0, 0, cell, cell);
-        context.beginPath();
-        context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
-        context.fill();
+        context.fillText(glyph, 0, 0);
         
         context.restore(); 
     }
   };
 };
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '.';
+  if (v < 150) return '-';
+  if (v < 200) return '+';
+
+  const glyphs = '_= /'.split('');
+
+  return random.pick(glyphs);
+};
+
 //function to render letter from keyboad click
 const onKeyUp = (e) => { 
  // console.log(e);
